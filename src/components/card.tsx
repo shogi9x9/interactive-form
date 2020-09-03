25ce90591e3abb52b3ecd63a8fdca65ea3719a32
shogi9x9 <ryo.ueshige@mail.utoronto.ca>
2020-09-03 16:23:33 -0400

    updated card view

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -28,13 +28,44 @@ export default class Card extends React.Component<{}, cardState> {
   }
 
   render() {
+    const cardNumberSection = this.getCardNumberSectoin();
     return (
       <div className='p-card'>
         <div className='p-card__top'>
           <img src='/chip.svg' alt='chip_logo'/>
           <img src='/visa_logo.svg' alt='visa_logo' className='p-card__top-visaLogo'/>
         </div>
+        <div className='p-card__cardNumber'>
+          {cardNumberSection}
+        </div>
+        <div className='p-card__content'>
+          <div className='p-card__holderName'>
+            <p className='p-card__holderName-label'>Card Holder</p>
+            <p className='p-card__holderName-name'>FULL NAME</p>
+          </div>
+          <div className='p-card__expirationDate'>
+            0424
+          </div>
+        </div>
       </div>
     )
+  }
+
+  private getCardNumberSectoin() {
+    const cardNumberSection = [];
+    for(let i = 0; i < 4; i++) {
+      const cardNumberDigitBlock = []
+      for(let j = 0; j < 4; j++) {
+        cardNumberDigitBlock.push(
+            <span className='p-card__cardNumber-digit'>#</span>
+        );
+      }
+      cardNumberSection.push(
+        <div className='p-card__cardNumber-block'>
+          {cardNumberDigitBlock}
+        </div>
+      );
+    }
+    return cardNumberSection;
   }
 }
